6a0785b065be1f9ad94dfda92566ff2e4220c4c2
Tobias Meier <Tobias.Meier@bridging-it.de>
2018-11-15 14:23:47 +0000

    Zustandshafter Dienst hinzugefügt

---
+++ SvcFabricDinnerDemo/SvcFabricDinnerDemo.RestaurantService.Interfaces/RestaurantServiceProxyFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+
+namespace SvcFabricDinnerDemo.RestaurantService.Interfaces
+{
+    public class RestaurantServiceProxyFactory
+    {
+        private readonly Uri _servicename = new Uri("fabric:/SvcFabricDinnerDemo/SvcFabricDinnerDemo.RestaurantService");
+        public RestaurantServiceProxyFactory()
+        { }
+        public IRestaurantService CreateServiceProxy()
+        {
+            return ServiceProxy.Create<IRestaurantService>(this._servicename, new ServicePartitionKey(0));
+        }
+        public IRestaurantAdminService CreateAdminServiceProxy()
+        {
+            return ServiceProxy.Create<IRestaurantAdminService>(this._servicename, new ServicePartitionKey(0));
+        }
+    }
+}
+
