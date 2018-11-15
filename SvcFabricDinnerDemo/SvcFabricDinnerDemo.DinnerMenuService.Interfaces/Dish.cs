343e66a3e113b5ed3711766522b1a0351dda252b
Tobias Meier <Tobias.Meier@bridging-it.de>
2018-11-15 14:46:20 +0000

    DinnerMenuService

---
+++ SvcFabricDinnerDemo/SvcFabricDinnerDemo.DinnerMenuService.Interfaces/Dish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SvcFabricDinnerDemo.DinnerMenuService.Interfaces
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
+
+
+
+
