d1196ed33b7634737c31fb2cb8b86d459595d0be
Tobias Meier <Tobias.Meier@bridging-it.de>
2018-11-18 15:47:33 +0000

    Erster Mesh-Service

---
+++ DinnerMeshDemo/StorageService/Controllers/StorageController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StorageController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromBody] FoodOrder foodOrder)
+        {
+            System.Diagnostics.Debug.WriteLine(foodOrder.RestaurantId);
+            foreach (var item in foodOrder.Items)
+            {
+                System.Diagnostics.Debug.WriteLine(item);
+            }
+        }
+    }
+
+    public class FoodOrder
+    {
+        public Guid RestaurantId { get; set; }
+        public string [] Items { get; set; }
+    }
+}
