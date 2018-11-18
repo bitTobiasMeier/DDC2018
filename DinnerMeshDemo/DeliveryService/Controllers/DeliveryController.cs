4281900335f9a34b535e7a531a3459c94929626e
Tobias Meier <Tobias.Meier@bridging-it.de>
2018-11-18 16:15:09 +0000

    Deliver-Service hinzugefügt

---
+++ DinnerMeshDemo/DeliveryService/Controllers/DeliveryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DeliveryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeliveryController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] DeliverOrder deliverOrder)
+        {
+            System.Diagnostics.Debug.WriteLine(deliverOrder.Food);
+        }
+    }
+
+    public class DeliverOrder
+    {
+        public Guid RestaurantId { get; set; }
+        public string Food { get; set; }
+    }
+}
--- DinnerMeshDemo/StorageService/Controllers/StorageController.cs
+++ DinnerMeshDemo/StorageService/Controllers/StorageController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace StorageService.Controllers
@@ -17,13 +19,43 @@ namespace StorageService.Controllers
             foreach (var item in foodOrder.Items)
             {
                 System.Diagnostics.Debug.WriteLine(item);
+                DeliverAsync(foodOrder.RestaurantId, item);
             }
         }
+
+        private Task DeliverAsync(Guid restaurantId, string food)
+        {
+            var client = new HttpClient();
+            var order = new DeliverOrder() {
+                RestaurantId = restaurantId,
+                Food = food
+            };
+            using (var response = client.PostAsJsonAsync("http://deliveryservice.dinnermeshdemo:20004/api/Delivery", order).GetAwaiter().GetResult())
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    throw new Exception("Error:" + response.ReasonPhrase);
+                }
+            }
+
+        }
     }
+
+    
 
     public class FoodOrder
     {
         public Guid RestaurantId { get; set; }
         public string [] Items { get; set; }
+    }
+
+    public class DeliverOrder
+    {
+        public Guid RestaurantId { get; set; }
+        public string Food { get; set; }
     }
 }
